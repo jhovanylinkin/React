d921ab80f4a0759366681b9d45ba960d3afb419f
Jhovany Morales Bruno <jhovanymb.info@gmail.com>
2018-01-18 00:28:32 -0600

    Consumo de API with React

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const app = express();
 
 app.use(express.static(__dirname +'/public'));
 
+person = [
+    {
+        name: 'Alejandro',
+        lastname: 'Matus'
+    },
+    {
+        name:'Jhovany',
+        lastname:'Morales'
+    }
+]
+
+app.get('/api/people/',(req, resp)=>{
+    resp.status(200).json({person : person});
+});
+
+
 app.listen(3000,()=>{
     console.log(`server running on port 3000`);
 });
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import request from 'superagent';
+
 
 class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            users: []
+        }
+    }
+    componentDidMount(){
+        request.get('/api/people').end((err, resp)=>{
+            const users = JSON.parse(resp.text).person;
+            this.setState({
+                users: users
+            });
+        });
+    }
+
     render(){
+        var users = this.state.users.map((user,i)=>{
+            return <li key={i}>{user.name}</li>
+        });
         return(
-            <h1>Mi First component in React</h1>
+            <div>
+                <h1>React app</h1>
+                <ul>
+                    {users}
+                </ul>
+            </div>
         )
     }
 }
